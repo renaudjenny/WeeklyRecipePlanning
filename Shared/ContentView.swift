879a3b0ed9162a6ed0c778b0f61e20905890cfbb
Renaud Jenny <renaud.jenny@gmail.com>
2021-05-14 21:18:39 +0100

    feat(App): add TabView with Recipes and Week tabs

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,8 +6,19 @@ struct ContentView: View {
     let store: Store<AppState, AppAction>
 
     var body: some View {
-        NavigationView {
-            RecipeListView(store: store.scope(state: { $0.recipeList }, action: AppAction.recipeList))
+        TabView {
+            NavigationView {
+                RecipeListView(store: store.scope(state: { $0.recipeList }, action: AppAction.recipeList))
+            }
+            .tabItem {
+                Image(systemName: "text.book.closed")
+                Text("Recipes")
+            }
+            WeekView(store: store.scope(state: { $0.week }, action: AppAction.week))
+                .tabItem {
+                    Image(systemName: "calendar.badge.clock")
+                    Text("Week")
+                }
         }
     }
 }
