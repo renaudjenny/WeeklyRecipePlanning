8e5c38e7217461d0174374ee621ed1b9996175da
Renaud Jenny <renaud.jenny@gmail.com>
2021-05-18 19:14:45 +0100

    feat(RecipeView): improve IngredientRow UX

--- Shared/Recipe/Ingredient/IngredientRow.swift
+++ Shared/Recipe/Ingredient/IngredientRow.swift
@@ -6,23 +6,33 @@ struct IngredientRow: View {
 
     var body: some View {
         WithViewStore(store) { viewStore in
-            HStack {
+            Section {
                 TextField("Name", text: viewStore.binding(get: { $0.name }, send: IngredientAction.nameChanged))
                     .font(.title2)
-                TextField("Quantity", text: viewStore.binding(get: { $0.quantity.text }, send: IngredientAction.quantityChanged))
-                //                    .keyboardType(.decimalPad)
-                Button { } label: {
+                HStack {
+                    TextField("Quantity", text: viewStore.binding(get: { $0.quantity.text }, send: IngredientAction.quantityChanged))
+                        .scaledToFit()
+                    //                    .keyboardType(.decimalPad)
                     Text(viewStore.unit?.symbol ?? "-")
-                }
-                .onTapGesture { viewStore.send(.unitButtonTapped, animation: .default) }
-            }
-            if viewStore.isUnitInEditionMode {
-                Picker("Unit", selection: viewStore.binding(get: { $0.unit }, send: IngredientAction.unitChanged)) {
-                    ForEach(RecipeUnit.allCases) { unit in
-                        Text(unit.text).tag(unit.rawValue)
+                    Spacer()
+                    Button { } label: {
+                        Text(
+                            viewStore.isUnitInEditionMode
+                            ? "Edit unit"
+                            : "Close"
+                        )
                     }
+                    .onTapGesture { viewStore.send(.unitButtonTapped, animation: .default) }
                 }
-                .pickerStyle(pickerStyle)
+
+                if viewStore.isUnitInEditionMode {
+                    Picker("Unit", selection: viewStore.binding(get: { $0.unit }, send: IngredientAction.unitChanged)) {
+                        ForEach(RecipeUnit.allCases) { unit in
+                            Text(unit.text).tag(unit.rawValue)
+                        }
+                    }
+                    .pickerStyle(pickerStyle)
+                }
             }
         }
     }
--- Shared/Recipe/RecipeView.swift
+++ Shared/Recipe/RecipeView.swift
@@ -12,9 +12,7 @@ struct RecipeView: View {
                         .font(.title)
                 }
 
-                VStack(alignment: .leading) {
-                    Stepper("Meal count: \(viewStore.mealCount)", value: viewStore.binding(get: { $0.mealCount }, send: RecipeAction.mealCountChanged), in: 0...99)
-                }
+                Stepper("Meal count: \(viewStore.mealCount)", value: viewStore.binding(get: { $0.mealCount }, send: RecipeAction.mealCountChanged), in: 0...99)
 
                 IngredientListView(store: store.scope(state: { $0.ingredientList }, action: RecipeAction.ingredientList))
             }
